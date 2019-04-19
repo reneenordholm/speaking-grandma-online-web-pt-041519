03a8e2bde794f013abc700a4df13555c2ed99127
Renee Nordholm <reneenordholm@gmail.com>
2019-04-19 14:47:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase 
-    return "NO, NOT SINCE 1938!"
-  else
+  elsif phrase != .upcase 
     return "HUH?! SPEAK UP, SONNY!"
+  else
+    return 
     
  end   
 end
