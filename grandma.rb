7ec2114ae3bee952e8ec5d4bc7b7cd4db34d2092
Renee Nordholm <reneenordholm@gmail.com>
2019-04-19 14:41:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(phrase)
   elsif phrase.upcase 
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
-    return 
+    return "NO, NOT SINCE 1938!"
  end   
 end
